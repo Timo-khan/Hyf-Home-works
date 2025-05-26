9143938435d7f5ad534393d46e08ecb100d8dadc
Muhammed khan <timokhan2@gmail.com>
2025-05-26 22:20:35 +0200

    Delete week-1 directory

--- week-1/age-calculation.js
+++
@@ -1,6 +0,0 @@
-//Future years calculation
-const yearOfBirth = 1991;
-let yearFuture = 2027;
-let age = 2025-1991;
-yearFuture = 2027-1991;
-console.log("You will be " + yearFuture + " years old in 2027" + ".")
