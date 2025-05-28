5ff6a63e60048200735df9d5ecfd2f3114aada32
Timo <timokhan2@gmail.com>
2025-05-29 00:21:46 +0200

    comments added

--- minimist.js
+++ minimist.js
@@ -1,3 +1,4 @@
+//Parsing arguments using Minimist module
 1
 2
 3
@@ -10,3 +11,6 @@ const args = require('minimist')(process.argv.slice(2));
  
 console.log(args.i);
 console.log(args.n);
+
+
+//code to execute "$node minimist.js –i node –n 15"
