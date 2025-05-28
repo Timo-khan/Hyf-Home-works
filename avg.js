f3c993397d6fd8da36f810082be681caba64b729
Timo <timokhan2@gmail.com>
2025-05-29 01:06:00 +0200

    code added to catch error when the provided arguments are not numbers

--- avg.js
+++ avg.js
@@ -1,5 +1,8 @@
 const args = process.argv.slice(2);
 const numbers = args.map(Number);
+if (numbers.some(isNaN)) {
+    console.error("Error: Not a Number! please try again");
+}
 const total = numbers.reduce((a, b) => a + b, 0);
 const average = total / numbers.length;
 console.log(average);
