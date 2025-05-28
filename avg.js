5535dbcc3cc502f9e023cf6ffa7e72dcb1dd31a4
Timo <timokhan2@gmail.com>
2025-05-29 01:01:45 +0200

    console.log added

--- avg.js
+++ avg.js
@@ -2,3 +2,4 @@ const args = process.argv.slice(2);
 const numbers = args.map(Number);
 const total = numbers.reduce((a, b) => a + b, 0);
 const average = total / numbers.length;
+console.log(average);
