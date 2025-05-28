2e525a6c2514fef47cd010069aa5c1db40ca1d08
Timo <timokhan2@gmail.com>
2025-05-29 01:28:00 +0200

    comments added

--- nodejs-week1/avg.js
+++ nodejs-week1/avg.js
@@ -9,3 +9,10 @@ if (args.length === 0) {
 const total = numbers.reduce((a, b) => a + b, 0);
 const average = total / numbers.length;
 console.log(average);
+
+
+
+
+// What should happen if the provided arguments = ( node avg.js 13 7 4)?
+// What should happen if the provided arguments are not numbers (node avg.js 1 two hello 4)?
+// What about if no arguments are provided (node avg.js)?
