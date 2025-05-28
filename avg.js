d612233632886af87659ec7b9557930dea9f9f32
Timo <timokhan2@gmail.com>
2025-05-29 01:07:12 +0200

    code added to catch error when no arguments are provided

--- avg.js
+++ avg.js
@@ -3,6 +3,9 @@ const numbers = args.map(Number);
 if (numbers.some(isNaN)) {
     console.error("Error: Not a Number! please try again");
 }
+if (args.length === 0) {
+    console.error("Error: Please provide an argument!");
+}
 const total = numbers.reduce((a, b) => a + b, 0);
 const average = total / numbers.length;
 console.log(average);
