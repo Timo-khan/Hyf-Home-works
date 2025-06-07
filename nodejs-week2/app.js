8c36c3cc4722593bd789cba6d3dfc5e66b9c5521
Timo <timokhan2@gmail.com>
2025-06-08 00:30:03 +0200

    query search and filtering + route for getting a document by its ID

--- nodejs-week2/app.js
+++ nodejs-week2/app.js
@@ -10,9 +10,45 @@ app.get('/search', (req, res) => {
     const query = req.query.q;
     if (!query) {
     res.send(documents);
-    return;
+    }
+
+const lowerCaseQuery = query.toLowerCase(); 
+const results = documents.filter(doc => {
+    return Object.values(doc).some(value => {
+        if (typeof value === 'string') {
+        return value.toLowerCase().includes(lowerCaseQuery);
+    }
+        return false;
+    });
+});
+
+    res.send(results);
+});
+
+
+app.get('/documents/:id', (req, res) => {
+const id = parseInt(req.params.id);
+
+const document = documents.find(document => document.id === id);
+
+    if (document) {
+    res.json(document);
+    } else {
+    res.status(404).json({ error: 'Document not found' });
     }
 });
+
+app.post 
+('/search', (req, res) =>{
+    const query = req.query.q?.toLowerCase();
+
+    if (query) {
+    results = results.filter(doc => {
+    return Object.values(doc).some(value =>
+    typeof value === 'string' && value.toLowerCase().includes(query));
+    });
+    }
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
