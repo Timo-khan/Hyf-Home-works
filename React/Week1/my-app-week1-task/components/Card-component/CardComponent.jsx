1bb0a076482652f09d7c9840eb6e652768022c05
Timo <timokhan2@gmail.com>
2025-06-19 00:04:26 +0200

    card component added

---
+++ React/Week1/my-app-week1-task/components/Card-component/CardComponent.jsx
@@ -0,0 +1,9 @@
+import "./CardComponent.css"
+export const CardComponent = ()=> {
+    return (
+        <div className = "myClass">
+            <h2>Card Title</h2>
+            <p>This is a card component</p>
+        </div>
+    )
+}
